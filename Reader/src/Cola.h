b2224f9367047b2109cee32442da5fc62b8f6cf3
olman <olmancp95@gmail.com>
2018-05-14 16:35:37 -0600

    Reader 0.0.2

---
+++ Reader/src/Cola.h
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct Nodo
+{
+
+  int pid;
+  int burst;
+  int prioridad;
+  pthread_t hilo;
+  struct Nodo *siguiente;
+  struct Nodo *anterior;
+
+}Nodo;
+
+typedef struct Cola{
+	struct Nodo *fin;
+	struct Nodo *inicio;
+}Cola;
+
+
+void insertar(Cola *cola, int dato_id,int dato_pid,int dato_burst,int dato_prioridad)
+{
+  Nodo *dato;
+  dato=(Nodo*)malloc(sizeof(Nodo));
+  dato->pid=dato_pid;
+  dato->burst=dato_burst;
+  dato->prioridad= dato_prioridad;
+  dato->siguiente=NULL;
+
+
+  if(cola->fin==NULL){
+	  cola->fin=dato;
+	  cola->inicio=dato;
+
+  }else{
+	dato->anterior=cola->fin;
+	cola->fin->siguiente=dato;
+	cola->fin=dato;
+  }
+}
+
+
+void imprimir(Cola *cola){
+	Nodo *actual;
+	actual = cola->inicio;
+
+	while(actual != NULL){
+		printf("PID: %d \t Burst: %d \t Prioridad: %d \n",actual->pid,actual->burst,actual->prioridad);
+		actual = actual->siguiente;
+	}
+
+}
--- Reader/src/Reader.c
+++ Reader/src/Reader.c
@@ -7,11 +7,33 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <fcntl.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int len = sizeof(int )*100;
+	key_t shmkey=0x1234567;
+	int shmid;
+
+    shmid = shmget (shmkey, len, 0644 | IPC_EXCL);
+    if (shmid < 0){
+        perror ("shmget\n");
+        exit (1);
+    }
+
+
+    int *sms;
+    sms = shmat(shmid, NULL, 0);
+    //printf("PID= %d \t Estado= %d \n",sms[0],sms[1]);
+
+    for(int i=0;i<20;i++){
+		if(sms[i*5+1]==1){
+			printf("PID= %d \t Estado= %d \n",sms[i*5],sms[i*5+1]);
+		}
+	}
 	return EXIT_SUCCESS;
 }

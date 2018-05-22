dda4917622ab2df5d2c2c8fdef56138da560f7cf
olman <olmancp95@gmail.com>
2018-05-21 19:38:19 -0600

    Reader 0.0.3

--- Reader/src/Reader.c
+++ Reader/src/Reader.c
@@ -9,31 +9,116 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/shm.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <semaphore.h>
+#include <pthread.h>
 
-int main(void) {
-	int len = sizeof(int )*100;
-	key_t shmkey=0x1234567;
-	int shmid;
+#include "Cola.h"
+#include "CrearLector.h"
+#include "Hilo.h"
+#include "Semaforo.h"
 
-    shmid = shmget (shmkey, len, 0644 | IPC_EXCL);
-    if (shmid < 0){
+int main(int argc, int *argv[]) {
+	//datos iniciales
+	int cantidad;
+	int tiempo_dormir;
+	int tiempo_escribir;
+	int final=1;
+	int linea_actual=0;
+	int *plinea_actual=linea_actual;
+	int *pfinal=&final;
+
+	//parametros
+	if(argc>2){
+		cantidad=strtol(argv[1], NULL, 10);
+		tiempo_escribir=strtol(argv[1], NULL, 10);
+		tiempo_dormir=strtol(argv[1], NULL, 10);
+	}else{
+		cantidad = 2;
+		tiempo_escribir=2;
+		tiempo_escribir=2;
+	}
+	//cola
+	Cola * cola = (Cola*)malloc(sizeof(Cola));
+
+
+
+	//memoria compartida
+	int largo_3 = sizeof(int )*2;
+	key_t shmkey_3=0x1234563;
+	int shmid_3;
+    int * largo;
+	shmid_3 = shmget (shmkey_3, largo_3, 0664 | IPC_CREAT);
+    if (shmid_3 < 0){
         perror ("shmget\n");
         exit (1);
     }
+    largo = (int *)shmat(shmid_3, NULL, 0);
 
 
+    int largo_2 = largo[1];
+    key_t shmkey_2=0x1234562;
+	int shmid_2;
+    int * estado;
+	shmid_2 = shmget (shmkey_2, largo_2, 0664 | IPC_CREAT);
+    if (shmid_2 < 0){
+        perror ("shmget\n");
+        exit (1);
+    }
+    estado = (int *)shmat(shmid_2, NULL, 0);
+	int largo_1 = largo[0];
+    key_t shmkey_1=0x1234561;
+	int shmid_1;
     int *sms;
-    sms = shmat(shmid, NULL, 0);
-    //printf("PID= %d \t Estado= %d \n",sms[0],sms[1]);
-
-    for(int i=0;i<20;i++){
-		if(sms[i*5+1]==1){
-			printf("PID= %d \t Estado= %d \n",sms[i*5],sms[i*5+1]);
-		}
+    shmid_1 = shmget (shmkey_1, largo_1, 0664 | IPC_CREAT);
+    if (shmid_1 < 0){
+		perror ("shmget\n");
+		exit (1);
 	}
+	sms = (int *)shmat(shmid_1, NULL, 0);
+
+	//semaforo
+    sem_t *psem_1=sem_open("SEM_1", 0);
+    sem_t *psem_2=sem_open("SEM_2", 0);
+
+    //crear procesos
+    sem_wait(psem_2);
+    int lineas=estado[1];
+	int largo_linea=estado[2];
+	int readers=cantidad;
+	int readers_max=estado[5];
+	int readers_init=estado[8];
+	sem_post(psem_2);
+	Configuracion * configuracion;
+	configuracion=crear_configuracion(lineas,largo_linea,readers,readers_max,readers_init,tiempo_dormir,tiempo_escribir);
+	crear_Lector(cola,cantidad,estado,sms,pfinal,psem_1,configuracion,linea_actual);
+
+
+    Nodo * actual;
+
+    actual=cola->inicio;
+    while(actual!=NULL){
+        iniciar_hilo(actual);
+    	actual=actual->siguiente;
+    }
+
+    cola->writers=cantidad;
+    cola->estado=estado;
+    cola->finalizar=pfinal;
+    cola->semaforo_1=psem_2;
+    iniciar_hilo_estado(cola);
+
+    actual=cola->inicio;
+    while(actual!=NULL){
+    	ejecutar_hilo(actual);
+    	actual=actual->siguiente;
+
+    }
+    ejecutar_mostrar_estado(cola);
+
 	return EXIT_SUCCESS;
 }
---
+++ Reader/src/Semaforo.h
@@ -0,0 +1,75 @@
+void estado(Nodo *nodo,int estado){
+	nodo->estado_actual=estado;
+	//nodo->estado[nodo->writers_init+nodo->pid*2]=nodo->pid;
+	//nodo->estado[nodo->writers_init+nodo->pid*2+1]=estado;
+}
+
+void semaforo(Nodo *nodo){
+	estado(nodo,1);
+	int cambio=1;
+    nodo->proceso++;
+	if(nodo->proceso==1){
+		sem_wait (nodo->semaforo_1);
+	}
+
+	for(int i=0;i<nodo->configuracion->lineas;i++){
+		if(cambio == 1){
+			//printf("la %d \n",nodo->linea_actual);
+			if(nodo->linea_actual == i){
+				nodo->linea_actual++;
+
+				if(nodo->sms[i*nodo->configuracion->largo_linea+2] == 1){
+					nodo->linea=nodo->sms[i*nodo->configuracion->largo_linea];
+					nodo->pid_mesajero=nodo->sms[i*nodo->configuracion->largo_linea+1];
+					//nodo->estado=nodo->sms[i*nodo->configuracion->largo_linea+2]; estado
+					nodo->hora=nodo->sms[i*nodo->configuracion->largo_linea+3];
+					nodo->minuto=nodo->sms[i*nodo->configuracion->largo_linea+4];
+					nodo->dia=nodo->sms[i*nodo->configuracion->largo_linea+5];
+					nodo->mes=nodo->sms[i*nodo->configuracion->largo_linea+6];
+					nodo->anno=nodo->sms[i*nodo->configuracion->largo_linea+7];
+					nodo->sms_1=nodo->sms[i*nodo->configuracion->largo_linea+8];
+					nodo->sms_2=nodo->sms[i*nodo->configuracion->largo_linea+9];
+					cambio=0;
+
+				}else{
+					estado(nodo,4);
+				}
+				if(nodo->configuracion->lineas == nodo->linea_actual){
+					nodo->linea_actual=0;
+
+				}
+			}else{
+				estado(nodo,4);
+			}
+
+		}else{
+			estado(nodo,4);
+		}
+
+	}
+
+	estado(nodo,2);
+	sleep(nodo->configuracion->tiempo_escribir);
+
+	nodo->proceso--;
+	if(nodo->proceso==0){
+		sem_post (nodo->semaforo_1);
+		sem_wait(nodo->semaforo_1);
+			for(int j=1;j<nodo->configuracion->lineas;j++){
+				nodo->sms[j*nodo->configuracion->largo_linea+1]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+2]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+3]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+4]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+5]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+6]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+7]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+8]=0;
+				nodo->sms[j*nodo->configuracion->largo_linea+9]=0;
+			}
+		sem_post(nodo->semaforo_1);
+
+	}
+
+	estado(nodo,3);
+	sleep(nodo->configuracion->tiempo_dormir);
+}
